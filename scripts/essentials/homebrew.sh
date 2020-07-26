67718ebded376bf9325bc09a3f91f20f5a9377af
James Fry <james.fry@jrfsolutions.co.uk>
2020-07-26 22:12:10 +0100

    Move homebrew back into main script

--- scripts/essentials/homebrew.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Install latest Homebrew package manager
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
--- setup-macosx.sh
+++ setup-macosx.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Install latest Homebrew package manager
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
 for script in scripts/essentials/*.sh scripts/tools/*.sh
 do
     sh ${script}
