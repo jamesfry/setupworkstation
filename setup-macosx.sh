efc631ae1725768410c5132e1bc28cc5a09c7fce
James Fry <james.fry@jrfsolutions.co.uk>
2020-07-26 14:14:59 +0100

    VSCode geo data

--- setup-macosx.sh
+++ setup-macosx.sh
@@ -171,12 +171,14 @@ for extension in \
     eg2.vscode-npm-script \
     foxundermoon.shell-format \
     hashicorp.terraform \
+    jumpinjackie.vscode-map-preview \
     kumar-harsh.graphql-for-vscode \
     mechatroner.rainbow-csv \
     mohsen1.prettify-json \
     ms-azuretools.vscode-docker \
     ms-vscode-remote.remote-containers \
     passionkind.prettier-vscode-with-tabs \
+    RandomFractalsInc.geo-data-viewer \
     vscjava.vscode-maven
 do
     code --install-extension $extension
