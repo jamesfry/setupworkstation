4003b803915fba58b8e740a57fb867839b5e5770
James Fry <james.fry@jrfsolutions.co.uk>
2022-12-26 14:32:01 +0000

    docker brew packages include completion

--- scripts/tools/docker.sh
+++ scripts/tools/docker.sh
@@ -4,15 +4,9 @@
 # Using the cask gets the dmg from the docker store and gives the gui tools
 brew install --cask docker
 
-# Docker compose
-brew install docker-compose
-
 # docker alternative method
 #brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
 #sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 #sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 #docker-machine create default --driver xhyve --xhyve-experimental-nfs-share
 
-# docker / docker machine completion
-brew install docker-completion
-brew install docker-machine-completion
