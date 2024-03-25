01bde228e78fac6f37877d992ffdbf2105eaff02
James Fry <james.fry@jrfsolutions.co.uk>
2024-03-25 11:52:24 +0000

    fix(mac-app-store): add mas command line tool before install scripts

--- setup-macosx.sh
+++ setup-macosx.sh
@@ -3,13 +3,11 @@
 # Install latest Homebrew package manager
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+brew install mas
+
 for script in scripts/essentials/*.sh scripts/tools/*.sh
 do
     sh ${script}
 done
 
-
 rsync -av --ignore-existing --progress dotfiles/ ~/
-
-brew install mas
-
